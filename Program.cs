f471d43f3382d2ba9b29214f9ba8f88b48278b9c
Mubarak <getmub@gmail.com>
2017-08-14 14:26:05 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter DeviceId");
+            string deviceId = Console.ReadLine();
+
+            var querystringData = new Dictionary<string, string>();
+            querystringData.Add("connectionType", "device");
+            querystringData.Add("deviceId", deviceId);
+            var connection = new HubConnection("http://relayserver.azurewebsites.net/", querystringData);
+            var myHub = connection.CreateHubProxy("IOTHub");
+            connection.Start().ContinueWith(task =>
+            {
+                if (task.IsFaulted)
+                {
+                    Console.WriteLine("There was an error opening the connection:{0}",
+                                      task.Exception.GetBaseException());
+                }
+                else
+                {
+                    Console.WriteLine("Connected");
+                }
+
+            }).Wait();
+
+
+            myHub.On<string>("onCommand", ( message) =>
+            {
+                Console.WriteLine("received cmd : {0}", message);
+            });
+
+            string deviceEvent = "";
+            do
+            {
+                Console.WriteLine("Enter Event to Hub (type close to disconnect) :");
+                deviceEvent = Console.ReadLine();
+                myHub.Invoke<string>("onDeviceEvent", deviceId, deviceEvent).ContinueWith(task =>
+                {
+                    if (task.IsFaulted)
+                    {
+                        Console.WriteLine("There was an error calling send: {0}",
+                                          task.Exception.GetBaseException());
+                    }
+                    else
+                    {
+                        Console.WriteLine(task.Result);
+                    }
+                });
+
+            } while (!deviceEvent.Equals("close"));
+
+            connection.Stop();
+        }
+    }
+}
